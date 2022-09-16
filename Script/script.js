99024ebd135c2de38911b8b7c61b930e5e06ebcc
Jhulan41 <jhulan41@gmail.com>
2022-09-17 00:46:23 +0530

    skill-section

--- Script/script.js
+++ Script/script.js
@@ -24,7 +24,7 @@ if(navClose){
 
 
 
-/* remove menu on mobile */
+/* =====================remove menu on mobile======================= */
 
 const navLink = document.querySelectorAll('.nav_link')
 
@@ -33,4 +33,25 @@ function linkAction(){
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*================================ Skills on-off=======================*/
+
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header');
+
+      function toggleSkills(){
+        let itemClass = this.parentNode.className;
+
+        for(let i = 0; i<skillsContent.length; i++){
+            skillsContent[i].className = 'skills_content skills_close';
+        }
+
+        if(itemClass === 'skills_content skills_close'){
+            this.parentNode.className = 'skills_content skills_open'
+        }
+      }
+
+      skillsHeader.forEach((el)=>{
+        el.addEventListener('click', toggleSkills)
+      })
