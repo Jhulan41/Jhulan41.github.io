21ae51c16e195df34ff80e719cf9cf77bea828a9
Jhulan41 <jhulan41@gmail.com>
2022-09-18 00:16:19 +0530

    Qualification section

--- Script/script.js
+++ Script/script.js
@@ -55,3 +55,24 @@ const skillsContent = document.getElementsByClassName('skills_content'),
       skillsHeader.forEach((el)=>{
         el.addEventListener('click', toggleSkills)
       })
+
+    /*===========================Qualification Tab===========================*/
+
+    const tabs = document.querySelectorAll('[data-target]');
+    const tabContents=document.querySelectorAll('[data-content]');
+
+    tabs.forEach((tab)=>{
+        tab.addEventListener('click', ()=>{
+            const target = document.querySelector(tab.dataset.target);
+
+            tabContents.forEach(tabContent =>{
+                tabContent.classList.remove('qualification_active');
+            })
+            target.classList.add('qualification_active');
+
+            tabs.forEach(tab =>{
+                tab.classList.remove('qualification_active');
+            })
+            tab.classList.add('qualification_active');
+        })
+    })
